896cf080b92d39e71025603a401b9a87784e62e7
Cnd96 <chamalnishageeth@gmail.com>
2024-02-06 10:24:15 +0530

    start v1

---
+++ src/start/classExample.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class ClassExample extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hi, Welcome to the React Training Session!</h2>
+        <h3>This is a react class component example.</h3>
+      </div>
+    );
+  }
+}
+
+export default ClassExample;
---
+++ src/start/functionalExample.js
@@ -0,0 +1,9 @@
+function FunctionalExample() {
+  return (
+    <div>
+      <h3>This is a react functional component example.</h3>
+    </div>
+  );
+}
+
+export default FunctionalExample;
